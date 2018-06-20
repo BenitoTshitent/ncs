6335215e592e544970b51fa3bca1d16262ea9d64
Benito muvum <muvumbenito@gmail.com>
2018-06-19 23:20:10 -0700

    save2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,272 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ncs</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+        <!--
+        CSS
+        ============================================= -->
+        <link rel="stylesheet" href="{{asset('css/linearicons.css')}}">
+        <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{'css/magnific-popup.css'}}">
+        <link rel="stylesheet" href="{{asset('css/nice-select.css')}}">
+        <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.carousel.css')}}">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
 
 
     </head>
     <body>
 
+        <header id="header" id="home">
+            <div class="container">
+                <div class="row align-items-center justify-content-between d-flex">
+                    <div id="logo">
+                        <a href="index.html"><img src="{{asset('img/logo.png')}}" alt="" title="" /></a>
+                    </div>
+                    <nav id="nav-menu-container">
+                        <ul class="nav-menu">
+                            <li class="menu-active"><a href="#home">Home</a></li>
+                            <li><a href="#offer">We Offer</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#project">Project</a></li>
+                            <li><a href="#price">Price</a></li>
+                        </ul>
+                    </nav><!-- #nav-menu-container -->
+                </div>
+            </div>
+        </header><!-- #header -->
+
+        <!-- start banner Area -->
+        <section class="banner-area" id="home">
+            <div class="container">
+                <div class="row fullscreen d-flex align-items-center justify-content-center">
+                    <div class="banner-content col-lg-7">
+                        <h1>
+                            All you want is here
+                        </h1>
+                        <p class="pt-20 pb-20">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End banner Area -->
+
+        <!-- Start we-offer Area -->
+        <section class="we-offer-area section-gap" id="offer">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="menu-content pb-60 col-lg-10">
+                        <div class="title text-center">
+                            <h1 class="mb-10">Our Offered Services</h1>
+                            <p>Who are in extremely love with eco friendly system.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="single-offer d-flex flex-row pb-30">
+                            <div class="icon">
+                                <img src="{{asset('img/s1.png')}}" alt="">
+                            </div>
+                            <div class="desc">
+                                <a href="#"><h4>Unlimited Colors</h4></a>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="single-offer d-flex flex-row pb-30">
+                            <div class="icon">
+                                <img src="{{asset('img/s3.png')}}" alt="">
+                            </div>
+                            <div class="desc">
+                                <a href="#"><h4>Endless Support</h4></a>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="single-offer d-flex flex-row pb-30">
+                            <div class="icon">
+                                <img src="{{asset('img/s2.png')}}" alt="">
+                            </div>
+                            <div class="desc">
+                                <a href="#"><h4>Smart Security</h4></a>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="single-offer d-flex flex-row pb-30">
+                            <div class="icon">
+                                <img src="{{asset('img/s4.png')}}" alt="">
+                            </div>
+                            <div class="desc">
+                                <a href="#"><h4>Smart Security</h4></a>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End we-offer Area -->
+
+        <!-- Start latest-blog Area -->
+        <section class="latest-blog-area section-gap" id="blog">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="menu-content pb-60 col-lg-8">
+                        <div class="title text-center">
+                            <h1 class="mb-10">Latest News from our Blog</h1>
+                            <p>Who are in extremely love with eco friendly system.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 single-blog">
+                        <img class="img-fluid" src="{{asset('img/b1.jpg')}}" alt="">
+                        <ul class="tags">
+                            <li><a href="#">Travel</a></li>
+                            <li><a href="#">Life style</a></li>
+                        </ul>
+                        <a href="#"><h4>Portable latest Fashion for young women</h4></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+                        </p>
+                        <p class="post-date">31st January, 2018</p>
+                    </div>
+                    <div class="col-lg-6 single-blog">
+                        <img class="img-fluid" src="img/b2.jpg" alt="">
+                        <ul class="tags">
+                            <li><a href="#">Travel</a></li>
+                            <li><a href="#">Life style</a></li>
+                        </ul>
+                        <a href="#"><h4>Portable latest Fashion for young women</h4></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+                        </p>
+                        <p class="post-date">31st January, 2018</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End latest-blog Area -->
+        <div class="whole-wrap">
+            <div class="container">
+                <div class="section-top-border">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-8">
+                            <h3 class="mb-30">Contacter nous</h3>
+                            <form action="#">
+                                <div class="mt-10">
+                                    <input type="text" name="first_name" placeholder="First Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'First Name'" required class="single-input">
+                                </div>
+                                <div class="mt-10">
+                                    <input type="text" name="last_name" placeholder="Last Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Last Name'" required class="single-input">
+                                </div>
+                                <div class="mt-10">
+                                    <input type="text" name="last_name" placeholder="Last Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Last Name'" required class="single-input">
+                                </div>
+                                <div class="mt-10">
+                                    <input type="email" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required class="single-input">
+                                </div>
+                                <div class="input-group-icon mt-10">
+                                    <div class="icon"><i class="fa fa-thumb-tack" aria-hidden="true"></i></div>
+                                    <input type="text" name="address" placeholder="Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Address'" required class="single-input">
+                                </div>
+                                <div class="mt-10">
+                                    <textarea class="single-textarea" placeholder="Message" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Message'" required></textarea>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- start footer Area -->
+        <footer class="footer-area section-gap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3  col-md-12">
+                        <div class="single-footer-widget">
+                            <h6>NOs contacts</h6>
+                            <ul class="footer-nav">
+                                <li><a href="#">Managed Website</a></li>
+                                <li><a href="#">Manage Reputation</a></li>
+                                <li><a href="#">Power Tools</a></li>
+                                <li><a href="#">Marketing Service</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6  col-md-12">
+                        <div class="single-footer-widget newsletter">
+                            <h6>Newsletter</h6>
+                            <p>You can trust us. we only send promo offers, not a single spam.</p>
+                            <div id="mc_embed_signup">
+                                <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="form-inline">
+
+                                    <div class="form-group row" style="width: 100%">
+                                        <div class="col-lg-8 col-md-12">
+                                            <input name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '" required="" type="email">
+                                            <div style="position: absolute; left: -5000px;">
+                                                <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-4 col-md-12">
+                                            <button class="nw-btn primary-btn">Subscribe<span class="lnr lnr-arrow-right"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="info"></div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row footer-bottom d-flex justify-content-between">
+                    <p class="col-lg-8 col-sm-12 footer-text m-0 text-white">
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+                    <div class="col-lg-4 col-sm-12 footer-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-behance"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End footer Area -->
+
+        <script src="{{asset('js/vendor/jquery-2.2.4.min.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+        <script src="{{asset('js/easing.min.js')}}"></script>
+        <script src="{{asset('js/hoverIntent.js')}}"></script>
+        <script src="{{asset('js/superfish.min.js')}}"></script>
+        <script src="{{asset('js/jquery.ajaxchimp.min.js')}}"></script>
+        <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('js/jquery.sticky.js')}}"></script>
+        <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+        <script src="{{asset('js/parallax.min.js')}}"></script>
+        <script src="{{asset('js/mail-script.js')}}"></script>
+        <script src="{{asset('js/main.js')}}"></script>
     </body>
 </html>
